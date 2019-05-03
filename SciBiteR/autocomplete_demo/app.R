d854e13066c3b0cfa7ea8cd3fe18dbbff8708807
Rachael Turner <rachaellturner92@gmail.com>
2019-05-03 11:24:56 +0100

    Formatting and VOCab input

--- SciBiteR/autocomplete_demo/app.R
+++ SciBiteR/autocomplete_demo/app.R
@@ -1,33 +1,43 @@
 library(shiny)
 
+appDesc <- "This is SciBite's autocomplete demo. Please ensure that the SPECIES VOCab is
+loaded into your TERMite server before continuing. Start typing into the text box to see the
+autocomplete options appear in the dropdown (e.g. \"mous\")."
+
 # UI
 ui <- shiny::fluidPage(
   titlePanel("SciBite Autocomplete Demo"),
-  mainPanel("This is SciBite's autocomplete demo, using the \"SPECIES\" VOCab. Please ensure that the SPECIES VOCab is
-            loaded into your TERMite server before continuing. Start typing into the text box to see the autocomplete
-            options appear in the dropdown (e.g. \"mous\")."),
-  textInput(inputId = 'text', label = 'text'),
-  uiOutput("autobox"),
-  textOutput("autoid")
+  sidebarLayout(position = "right",
+                sidebarPanel(uiOutput("autobox"), textOutput("autoid")),
+                mainPanel(p("To use this demo please type a VOCab code into the",
+                            strong("VOCab"),
+                            "textbox, then start typing in the",
+                            strong("Text"),
+                            "textbox."),
+                          em("As an example, use \"HUCELL\" as the VOCab and start typing \"macro\"."),
+                          div("Only VOCabs preloaded into your TERMite server can be used.", style = "color:red"),
+                          textInput(inputId = 'VOCabInput', label = 'VOCab'),
+                          textInput(inputId = 'text', label = 'Text'))
+  )
 )
 
 # Server
-server <- function(input, output, session) {
+server <- function(input, VOCab, output, session) {
   autotext <- shiny::reactive({ input$text })
+  VOCabtext <- shiny::reactive({ input$VOCabInput })
   output$autobox <- shiny::renderUI(
     if (nchar(autotext()) < 3) {
       shiny::selectInput(inputId = 'autobox', label = 'Autocomplete', choices = autotext())
-    }
-    else {
+    } else {
       auto <- SciBiteR::autocomplete(endpoint <- "http://localhost:9090/termite/toolkit/autocomplete.api",
-                           input <- autotext(),
-                           VOCab = "SPECIES",
-                           taxon = '')
+                                     input <- autotext(),
+                                     VOCab = toupper(VOCabtext()),
+                                     taxon = '')
       reactiveauto <- auto$id
       names(reactiveauto) <- auto$label
       shiny::selectInput(inputId = 'autobox', label = 'Autocomplete', choices = reactiveauto)
     }
-
+    
   )
   output$autoid <- shiny::renderText({ input$autobox })
 }
